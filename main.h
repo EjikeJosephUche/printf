dc885d674545b79c140f952c8e2c63b7787840f3
EjikeJosephUche <uche.ejike.123@gmail.com>
2023-10-19 17:15:05 -0700

    Updated

--- main.h
+++ main.h
@@ -51,6 +51,7 @@ int print_char(va_list types, char buffer[],
 int print_string(va_list types, char buffer[], int flags, int width, int precision, int size);
 int print_percent(va_list types, char buffer[],
 		int flags, int width, int precision, int size);
+int _putchar(char c);
 
 /* Functions to Print Numbers */
 int print_int(va_list types, char buffer[],
@@ -96,7 +97,7 @@ int handle_write_char(char c, char buffer[],
 		int flags, int width, int precision, int size);
 int write_number(int is_positive, int ind, char buffer[],
 		int flags, int width, int precision, int size);
-int write_num(int ind, char buffer[], int flags, int width, int precision, length, char padd, char extra_c);
+int write_num(int ind, char buffer[], int flags, int width, int precision, int length, char padd, char extra_c);
 int write_pointer(char buffer[], int ind, int length,
 		int width, int flags, char padd, char extra_c, int padd_start);
 int write_unsgnd(int is_negative, int ind, char buffer[],
